e3af6ba30765fa42ed26e28bb9d30baeb7f07db5
Fabián Hernández <fahg76@gmail.com>
2025-06-11 23:10:40 -0400

    Ya compila y se modifica código 3

--- src/main/java/com/equipo/saludo/App.java
+++ src/main/java/com/equipo/saludo/App.java
@@ -2,7 +2,7 @@ package com.equipo.saludo;
 
 public class App {
     public static String saludar(String nombre) {
-        return "¡Hola como estas, " + nombre + "!";
+        return "¡Hola, " + nombre + "!";
     }
 
     public static void main(String[] args) {
